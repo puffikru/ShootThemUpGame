ef14808a540f8d1fda0242b0d593c2a99a9fddba
Igor Bulakh <igorbulakh1987@yandex.ru>
2022-11-27 00:09:53 +0300

    Add Camera component and bind axis. Add SpringArmComponent and attach camera to it

--- Source/ShootThemUp/Public/Player/STUBaseCharacter.h
+++ Source/ShootThemUp/Public/Player/STUBaseCharacter.h
@@ -7,6 +7,7 @@
 #include "STUBaseCharacter.generated.h"
 
 class UCameraComponent;
+class USpringArmComponent;
 
 UCLASS()
 class SHOOTTHEMUP_API ASTUBaseCharacter : public ACharacter
@@ -22,6 +23,9 @@ protected:
 	virtual void BeginPlay() override;
 
 public:
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Components")
+    USpringArmComponent* SpringArmComponent;
+    
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Components")
     UCameraComponent* CameraComponent;
     
