98dc2caea837b227847bb6105b572f66c3c5f93a
Igor Bulakh <igorbulakh1987@yandex.ru>
2022-12-01 01:20:15 +0300

    Add GetMovementDirection for right/left/back animation calculation

--- Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
+++ Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
@@ -54,14 +54,26 @@ bool ASTUBaseCharacter::IsRunning() const
     return WantsToRun && IsMovingForward && !GetVelocity().IsZero();
 }
 
+float ASTUBaseCharacter::GetMovementDirection() const
+{
+    if (GetVelocity().IsZero()) return 0.0f;
+    const auto VelocityNormal = GetVelocity().GetSafeNormal();
+    const auto AngleBetween = FMath::Acos(FVector::DotProduct(GetActorForwardVector(), VelocityNormal));
+    const auto CrossProduct = FVector::CrossProduct(GetActorForwardVector(), VelocityNormal);
+    const auto Degrees = FMath::RadiansToDegrees(AngleBetween);
+    return  CrossProduct.IsZero() ? Degrees : Degrees * FMath::Sign(CrossProduct.Z);
+}
+
 void ASTUBaseCharacter::MoveForward(float Amount)
 {
+    if (Amount == 0.0f) return;
     AddMovementInput(GetActorForwardVector(), Amount);
     IsMovingForward = Amount > 0.0f;
 }
 
 void ASTUBaseCharacter::MoveRight(float Amount)
 {
+    if (Amount == 0.0f) return;
     AddMovementInput(GetActorRightVector(), Amount);
 }
 
--- Source/ShootThemUp/Public/Player/STUBaseCharacter.h
+++ Source/ShootThemUp/Public/Player/STUBaseCharacter.h
@@ -38,6 +38,9 @@ public:
     UFUNCTION(BlueprintCallable, Category="Movement")
     bool IsRunning() const;
 
+    UFUNCTION(BlueprintCallable, Category="Movement")
+    float GetMovementDirection() const;
+
 private:
     void MoveForward(float Amount);
     void MoveRight(float Amount);
